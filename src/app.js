07c31fe65b2a7832dce5b3e6a664a4b87fa3e662
kaychua <kaychua.exe@gmail.com>
2021-06-30 16:39:25 +1000

    axios and openWeather API structure added

---
+++ src/app.js
@@ -0,0 +1,4 @@
+apiKey = "616d4f1d4c8141d448674e1f1ec401a1";
+units = "metric";
+city = "Seoul";
+apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
