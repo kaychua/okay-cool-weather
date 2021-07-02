fe2bcda7ab60b695b828baf2aba5ed088334003c
kaychua <kaychua.exe@gmail.com>
2021-07-02 16:51:47 +1000

    weather forecast js created

--- src/app.js
+++ src/app.js
@@ -51,6 +51,33 @@ function displayTemperature(response) {
   );
 }
 
+function displayWeatherForecast() {
+  let weatherForecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+
+  let forecastHtml = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="col-2 weather-forecast">
+        <div class="weather-forecast-date">
+            ${day}
+        </div>
+        <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="weatherForecastConditionIcon" id="weather-forecast-icon"/>
+        <div class="weather-forecast-temperature">
+        <span id="max-forecast-temp">20°</span> |
+                <span id="min-forecast-temp"> 15°</span>
+              </div>
+              </div>
+        `;
+  });
+
+  forecastHtml = forecastHtml + `</div>`;
+  weatherForecastElement.innerHTML = forecastHtml;
+}
+
 function search(city) {
   let apiKey = "616d4f1d4c8141d448674e1f1ec401a1";
   let units = "metric";
@@ -94,3 +121,4 @@ let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", displayCelsiusUnit);
 
 search("Seoul");
+displayWeatherForecast();
